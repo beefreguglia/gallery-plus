7503f3f8be3aabd52f658c86f3fe1e5626a7c602
Bernardo <beefreguglia@gmail.com>
2025-09-18 17:52:05 -0300

    feature: config query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { LayoutMain } from "./pages/layout-main";
 import { PageComponents } from "./pages/page-components";
 import { PageHome } from "./pages/page-home";
 import { PagePhotoDetails } from "./pages/page-photo-details";
 
+const queryCient = new QueryClient();
+
 export default function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route element={<LayoutMain />}>
-					<Route index element={<PageHome />} />
-					<Route path="/components" element={<PageComponents />} />
-					<Route path="/photos/:id" element={<PagePhotoDetails />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<QueryClientProvider client={queryCient}>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<LayoutMain />}>
+						<Route index element={<PageHome />} />
+						<Route path="/components" element={<PageComponents />} />
+						<Route path="/photos/:id" element={<PagePhotoDetails />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	);
 }
