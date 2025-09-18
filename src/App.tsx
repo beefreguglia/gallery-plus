149c76aa0408e8313c80ad98669b1aadd6c518e7
Bernardo <beefreguglia@gmail.com>
2025-09-18 18:22:58 -0300

    feature: cofing nuqs in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NuqsAdapter } from "nuqs/adapters/react-router/v7";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { LayoutMain } from "./pages/layout-main";
 import { PageComponents } from "./pages/page-components";
@@ -10,15 +11,17 @@ const queryCient = new QueryClient();
 export default function App() {
 	return (
 		<QueryClientProvider client={queryCient}>
-			<BrowserRouter>
-				<Routes>
-					<Route element={<LayoutMain />}>
-						<Route index element={<PageHome />} />
-						<Route path="/components" element={<PageComponents />} />
-						<Route path="/photos/:id" element={<PagePhotoDetails />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+			<NuqsAdapter>
+				<BrowserRouter>
+					<Routes>
+						<Route element={<LayoutMain />}>
+							<Route index element={<PageHome />} />
+							<Route path="/components" element={<PageComponents />} />
+							<Route path="/photos/:id" element={<PagePhotoDetails />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</NuqsAdapter>
 		</QueryClientProvider>
 	);
 }
